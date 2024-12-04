36cd8dbc6d04e2451e12d29479c3c262e87c9647
jvecodev <jvitor.oliveira1803@gmail.com>
2024-12-04 12:16:29 -0300

    Primeiro exercicio

---
+++ Projeto/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> Tela em branco e o famoso Olá mundo!</Text>
+      <Text> Meu primeiro Projeto</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
